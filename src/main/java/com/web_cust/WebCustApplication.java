0f52407e5c12c587032ceae9cfb3313ce913746d
lilyindr <lily@ppu.co.id>
2024-12-18 11:22:02 +0700

    first time

---
+++ src/main/java/com/web_cust/WebCustApplication.java
@@ -0,0 +1,13 @@
+package com.web_cust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web_cust/WebCustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_cust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
